1776981065807242a5555e8e03dc3f98d6112a29
Martin Asenov <talismanofkingdom@gmail.com>
2019-01-31 18:50:12 +0200

    Update TableRelations.sql

--- C# Databases Basics/TableRelations.sql
+++ C# Databases Basics/TableRelations.sql
@@ -42,3 +42,33 @@ VALUES
 	(1, 'Roberto',  43300.00, 102),
 	(2,	'Tom',	56100.00, 103),
 	(3,	'Yana',	60200.00, 101)
+
+-- Problem 2. One-To-Many Relationship
+CREATE TABLE Manufacturers
+(
+	ManufacturerID INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(15) NOT NULL,
+	EstablishedOn DATE NOT NULL
+)
+
+CREATE TABLE Models
+(
+	ModelID INT PRIMARY KEY IDENTITY(101, 1),
+	[Name] VARCHAR(15) NOT NULL,
+	ManufacturerID INT FOREIGN KEY REFERENCES Manufacturers(ManufacturerID)
+)
+
+INSERT INTO Manufacturers ([Name], EstablishedOn)
+VALUES
+	('BMW', '07/03/1916'),
+	('Tesla', '01/01/2003'),
+	('Lada', '01/05/1966')
+
+INSERT INTO Models ([Name], ManufacturerID)
+VALUES
+	('X1',	1),
+	('i6',	1),
+	('Model S',	2),
+	('Model X',	2),
+	('Model 3',	2),
+	('Nova',	3)
