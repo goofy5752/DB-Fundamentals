81b621a4473a041bfaee5e2d0430cc2849d00ea6
Martin Asenov <talismanofkingdom@gmail.com>
2019-02-01 21:01:04 +0200

    Update TableRelations.sql

--- C# Databases Basics/TableRelations.sql
+++ C# Databases Basics/TableRelations.sql
@@ -112,3 +112,20 @@ VALUES
 	(3,	103),
 	(2,	102),
 	(2,	103)
+
+--Problem 4. Self-Referencing
+CREATE TABLE Teachers
+(
+	TeacherID INT PRIMARY KEY IDENTITY(101, 1),
+	[Name] VARCHAR(15) NOT NULL,
+	ManagerID INT FOREIGN KEY REFERENCES Teachers(TeacherID)
+)
+
+INSERT INTO Teachers 
+VALUES
+	('John',	NULL),
+	('Maya',	106),
+	('Silvia',	106),
+	('Ted',		105),
+	('Mark',	101),
+	('Greta',	101)
