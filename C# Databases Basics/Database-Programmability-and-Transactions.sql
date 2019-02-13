57311b6555e89f956bdf914e041b4d4236fe2600
Martin Asenov <talismanofkingdom@gmail.com>
2019-02-13 14:33:35 +0200

    Update Database-Programmability-and-Transactions.sql

--- C# Databases Basics/Database-Programmability-and-Transactions.sql
+++ C# Databases Basics/Database-Programmability-and-Transactions.sql
@@ -189,3 +189,232 @@ SELECT SUM(k.Cash) AS TotalCash
 --                  Part 1. Queries for Bank Database
 
 -- Problem 14. Create Table Logs
+CREATE TRIGGER InsertNewEntryIntoLogs
+  ON Accounts
+  AFTER UPDATE
+AS
+  INSERT INTO Logs
+  VALUES (
+    (SELECT Id
+     FROM inserted),
+    (SELECT Balance
+     FROM deleted),
+    (SELECT Balance
+     FROM inserted)
+  )
+
+-- Problem 15. Create Table Emails
+CREATE TRIGGER CreateNewNotificationEmail
+  ON Logs
+  AFTER INSERT
+AS
+  BEGIN
+    INSERT INTO NotificationEmails
+    VALUES (
+      (SELECT AccountId
+       FROM inserted),
+      (CONCAT('Balance change for account: ', (SELECT AccountId
+                                               FROM inserted))),
+      (CONCAT('On ', (SELECT GETDATE()
+                      FROM inserted), 'your balance was changed from ', (SELECT OldSum
+                                                                         FROM inserted), 'to ', (SELECT NewSum
+                                                                                                 FROM inserted), '.'))
+    )
+  END
+
+-- Problem 16. Deposit Money
+CREATE PROCEDURE usp_DepositMoney(@AccountId INT, @MoneyAmount MONEY)
+AS
+  BEGIN TRANSACTION
+  UPDATE Accounts
+  SET Balance += @MoneyAmount
+  WHERE Id = @AccountId
+  COMMIT
+
+-- Problem 17. Withdraw Money Procedure
+CREATE PROCEDURE usp_WithdrawMoney(@AccountId INT, @MoneyAmount MONEY)
+AS
+  BEGIN TRANSACTION
+  UPDATE Accounts
+  SET Balance -= @MoneyAmount
+  WHERE Id = @AccountId
+  COMMIT
+
+-- Problem 18. Money Transfer
+CREATE PROCEDURE usp_TransferMoney(@SenderId INT, @ReceiverId INT, @Amount DECIMAL(15, 4))
+AS
+  BEGIN
+    BEGIN TRANSACTION
+    EXEC dbo.usp_WithdrawMoney @SenderId, @Amount
+    EXEC dbo.usp_DepositMoney @ReceiverId, @Amount
+    IF ((SELECT Balance
+         FROM Accounts
+         WHERE Accounts.Id = @SenderId) < 0)
+      BEGIN
+        ROLLBACK
+      END
+    ELSE
+      BEGIN
+        COMMIT
+      END
+  END
+
+-- Problem 20. *Massive Shopping
+DECLARE @gameName NVARCHAR(50) = 'Safflower'
+DECLARE @username NVARCHAR(50) = 'Stamat'
+
+DECLARE @userGameId INT = (
+  SELECT ug.Id
+  FROM UsersGames AS ug
+    JOIN Users AS u
+      ON ug.UserId = u.Id
+    JOIN Games AS g
+      ON ug.GameId = g.Id
+  WHERE u.Username = @username AND g.Name = @gameName)
+
+DECLARE @userGameLevel INT = (SELECT Level
+                              FROM UsersGames
+                              WHERE Id = @userGameId)
+DECLARE @itemsCost MONEY, @availableCash MONEY, @minLevel INT, @maxLevel INT
+
+SET @minLevel = 11
+SET @maxLevel = 12
+SET @availableCash = (SELECT Cash
+                      FROM UsersGames
+                      WHERE Id = @userGameId)
+SET @itemsCost = (SELECT SUM(Price)
+                  FROM Items
+                  WHERE MinLevel BETWEEN @minLevel AND @maxLevel)
+
+IF (@availableCash >= @itemsCost AND @userGameLevel >= @maxLevel)
+
+  BEGIN
+    BEGIN TRANSACTION
+    UPDATE UsersGames
+    SET Cash -= @itemsCost
+    WHERE Id = @userGameId
+    IF (@@ROWCOUNT <> 1)
+      BEGIN
+        ROLLBACK
+        RAISERROR ('Could not make payment', 16, 1)
+      END
+    ELSE
+      BEGIN
+        INSERT INTO UserGameItems (ItemId, UserGameId)
+          (SELECT
+             Id,
+             @userGameId
+           FROM Items
+           WHERE MinLevel BETWEEN @minLevel AND @maxLevel)
+
+        IF ((SELECT COUNT(*)
+             FROM Items
+             WHERE MinLevel BETWEEN @minLevel AND @maxLevel) <> @@ROWCOUNT)
+          BEGIN
+            ROLLBACK;
+            RAISERROR ('Could not buy items', 16, 1)
+          END
+        ELSE COMMIT;
+      END
+  END
+
+SET @minLevel = 19
+SET @maxLevel = 21
+SET @availableCash = (SELECT Cash
+                      FROM UsersGames
+                      WHERE Id = @userGameId)
+SET @itemsCost = (SELECT SUM(Price)
+                  FROM Items
+                  WHERE MinLevel BETWEEN @minLevel AND @maxLevel)
+
+IF (@availableCash >= @itemsCost AND @userGameLevel >= @maxLevel)
+
+  BEGIN
+    BEGIN TRANSACTION
+    UPDATE UsersGames
+    SET Cash -= @itemsCost
+    WHERE Id = @userGameId
+
+    IF (@@ROWCOUNT <> 1)
+      BEGIN
+        ROLLBACK
+        RAISERROR ('Could not make payment', 16, 1)
+      END
+    ELSE
+      BEGIN
+        INSERT INTO UserGameItems (ItemId, UserGameId)
+          (SELECT
+             Id,
+             @userGameId
+           FROM Items
+           WHERE MinLevel BETWEEN @minLevel AND @maxLevel)
+
+        IF ((SELECT COUNT(*)
+             FROM Items
+             WHERE MinLevel BETWEEN @minLevel AND @maxLevel) <> @@ROWCOUNT)
+          BEGIN
+            ROLLBACK
+            RAISERROR ('Could not buy items', 16, 1)
+          END
+        ELSE COMMIT;
+      END
+  END
+
+SELECT i.Name AS [Item Name]
+FROM UserGameItems AS ugi
+  JOIN Items AS i
+    ON i.Id = ugi.ItemId
+  JOIN UsersGames AS ug
+    ON ug.Id = ugi.UserGameId
+  JOIN Games AS g
+    ON g.Id = ug.GameId
+WHERE g.Name = @gameName
+ORDER BY [Item Name]
+
+-- Problem 21. Employees With 3 Projects
+CREATE PROCEDURE usp_AssignProject(@employeeId INT, @projectID INT)
+AS
+  BEGIN
+    BEGIN TRAN
+    INSERT INTO EmployeesProjects
+    VALUES (@employeeId, @projectID)
+    IF (SELECT COUNT(ProjectID)
+        FROM EmployeesProjects
+        WHERE EmployeeID = @employeeId) > 3
+      BEGIN
+        RAISERROR ('The employee has too many projects!', 16, 1)
+        ROLLBACK
+        RETURN
+      END
+    COMMIT
+  END
+
+-- Problem 22.
+CREATE TABLE Deleted_Employees
+(
+  EmployeeId INT PRIMARY KEY IDENTITY,
+  FirstName VARCHAR(50) NOT NULL,
+  LastName VARCHAR(50) NOT NULL,
+  MiddleName VARCHAR(50),
+  JobTitle VARCHAR(50),
+  DepartmentId INT,
+  Salary DECIMAL(15, 2)
+)
+
+GO
+
+CREATE TRIGGER tr_DeleteEmployees
+  ON Employees
+  AFTER DELETE
+AS
+  BEGIN
+    INSERT INTO Deleted_Employees
+      SELECT
+        FirstName,
+        LastName,
+        MiddleName,
+        JobTitle,
+        DepartmentID,
+        Salary
+      FROM deleted
+  END
