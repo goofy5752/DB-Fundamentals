43f5d44358c9986e4f8dcaef13a3ec613e47aad8
Martin Asenov <talismanofkingdom@gmail.com>
2019-02-16 07:33:13 +0200

    Update MSSQL Server Exam - 10 Feb 2019.sql

--- C# Databases Basics/MSSQL Server Exam - 10 Feb 2019.sql
+++ C# Databases Basics/MSSQL Server Exam - 10 Feb 2019.sql
@@ -170,3 +170,16 @@ SELECT k.Id AS JourneyId, k.JobName
 	  JOIN TravelCards AS tc ON tc.JourneyId = j.Id
 	  JOIN Colonists AS c ON c.Id = tc.ColonistId) AS k
 	  WHERE k.RowNumber = 1
+
+-- 16. Select Second Oldest Important Colonist
+SELECT k.JobDuringJourney, c.FirstName + ' ' + c.LastName AS FullName, k.JobRank
+  FROM (
+  SELECT tc.JobDuringJourney AS JobDuringJourney, tc.ColonistId,
+DENSE_RANK() OVER (PARTITION BY tc.JobDuringJourney ORDER BY co.Birthdate ASC) AS JobRank
+  FROM TravelCards AS tc
+  JOIN Colonists AS co ON co.Id = tc.ColonistId
+  GROUP BY tc.JobDuringJourney, co.Birthdate, tc.ColonistId
+  ) AS k
+  JOIN Colonists AS c ON c.Id = k.ColonistId
+  WHERE k.JobRank = 2
+  ORDER BY k.JobDuringJourney
