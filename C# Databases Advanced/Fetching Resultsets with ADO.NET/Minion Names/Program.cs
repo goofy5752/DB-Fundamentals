057b8040c19b2fb145abb3066f38467a3d48c80f
Martin Asenov <talismanofkingdom@gmail.com>
2019-02-21 15:15:44 +0200

    Added New Tasks

---
+++ C# Databases Advanced/Fetching Resultsets with ADO.NET/Minion Names/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Minion_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ C# Databases Advanced/Fetching Resultsets with ADO.NET/Villain Names/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Villain_Names
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = Configuration.ConnectionString;
+
+            // Provide the query string with a parameter placeholder.
+            string queryString =
+                " SELECT v.Name, COUNT(mv.VillainId) AS MinionsCount " +
+                "FROM Villains AS v JOIN MinionsVillains AS mv ON v.Id = mv.VillainId " +
+                "GROUP BY v.Id, v.Name HAVING COUNT(mv.VillainId) > 3 " +
+                "ORDER BY COUNT(mv.VillainId)";
+
+            // Specify the parameter value.
+
+            // Create and open the connection in a using block. This
+            // ensures that all resources will be closed and disposed
+            // when the code exits.
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                // Create the Command and Parameter objects.
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+                // Open the connection in a try/catch block. 
+                // Create and execute the DataReader, writing the result
+                // set to the console window.
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("{0} - {1}",
+                            reader[0], reader[1]);
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
