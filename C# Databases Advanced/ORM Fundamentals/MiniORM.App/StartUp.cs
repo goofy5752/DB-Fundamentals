a4d43e6b69de13a621f8412bc91dab469a84f43a
Martin Asenov <talismanofkingdom@gmail.com>
2019-02-24 23:44:02 +0200

    Custom MiniORM

---
+++ C# Databases Advanced/ORM Fundamentals/MiniORM.App/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace MiniORM.App
+{
+    using System.Linq;
+    using Data;
+    using Data.Entities;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var connectionString = @"Server=DESKTOP-58UASJ8\SQLEXPRESS;Database=MiniORM;Integrated Security=True";
+
+            SoftUniDbContext db = new SoftUniDbContext(connectionString);
+
+            db.Employees.Add(new Employee
+                (
+                "Gosho",
+                "Inserted",
+                db.Departments.First().Id,
+                true
+                ));
+
+            Employee employee = db.Employees.Last();
+            employee.FirstName = "Modified";
+
+            db.SaveChanges();
+        }
+    }
+}
