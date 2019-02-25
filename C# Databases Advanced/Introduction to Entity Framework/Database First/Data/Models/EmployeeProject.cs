62e9bf0b1302c75779cf816dfdf45627a7d12a7d
Martin Asenov <talismanofkingdom@gmail.com>
2019-02-26 00:00:32 +0200

    New tasks added

---
+++ C# Databases Advanced/Introduction to Entity Framework/Database First/Data/Models/EmployeeProject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Database_First.Data.Models
+{
+    public partial class EmployeeProject
+    {
+        public int EmployeeId { get; set; }
+        public int ProjectId { get; set; }
+
+        public virtual Employees Employee { get; set; }
+        public virtual Projects Project { get; set; }
+    }
+}
---
+++ C# Databases Advanced/Introduction to Entity Framework/Database First/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Database_First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
