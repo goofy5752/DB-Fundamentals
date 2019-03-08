7dd62473b42770b60bbe22f6499d43481016f243
Martin Asenov <talismanofkingdom@gmail.com>
2019-03-08 14:11:25 +0200

    New tasks

---
+++ C# Databases Advanced/Code First/Hospital Database/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hospital_Database
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
