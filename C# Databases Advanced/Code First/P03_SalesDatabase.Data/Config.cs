a35b09c9195a6090e6eaf3438c852572cba37dc8
Martin Asenov <talismanofkingdom@gmail.com>
2019-03-11 23:04:34 +0200

    Sale Database added

---
+++ C# Databases Advanced/Code First/P03_SalesDatabase.Data/Config.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_SalesDatabase.Data
+{
+    public class Config
+    {
+        public const string SqlConnect = @"Server=DESKTOP-58UASJ8\SQLEXPRESS;Database=Sales;Integrated Security = True";
+    }
+}
---
+++ C# Databases Advanced/Code First/P03_SalesDatabase.Data/SalesContext.cs
@@ -0,0 +1,100 @@
+﻿namespace P03_SalesDatabase.Data
+{
+    using System;
+    using Microsoft.EntityFrameworkCore;
+    using Models;
+
+    public class SalesContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Store> Stores { get; set; }
+
+        public DbSet<Sale> Sales { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Config.SqlConnect);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            ConfigProductEntity(modelBuilder);
+
+            ConfigCustomerEntity(modelBuilder);
+
+            ConfigStoreEntity(modelBuilder);
+
+            ConfigSaleEntity(modelBuilder);
+        }
+
+        private void ConfigSaleEntity(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Sale>()
+                .HasKey(s => s.SaleId);
+        }
+
+        private void ConfigStoreEntity(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Store>()
+                .HasKey(x => x.StoreId);
+
+            modelBuilder
+                .Entity<Store>()
+                .HasMany(s => s.Sales)
+                .WithOne(s => s.Store);
+
+            modelBuilder
+                .Entity<Store>()
+                .Property(s => s.Name)
+                .HasMaxLength(80)
+                .IsUnicode();
+        }
+
+        private void ConfigCustomerEntity(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Customer>()
+                .HasKey(x => x.CustomerId);
+
+            modelBuilder
+                .Entity<Customer>()
+                .HasMany(c => c.Sales)
+                .WithOne(s => s.Customer);
+
+            modelBuilder
+                .Entity<Customer>()
+                .Property(x => x.Name)
+                .HasMaxLength(100)
+                .IsUnicode();
+
+            modelBuilder
+                .Entity<Customer>()
+                .Property(x => x.Email)
+                .HasMaxLength(80)
+                .IsUnicode(false);
+        }
+
+        private void ConfigProductEntity(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Product>()
+                .HasKey(x => x.ProductId);
+
+            modelBuilder
+                .Entity<Product>()
+                .Property(x => x.Name)
+                .HasMaxLength(50)
+                .IsUnicode();
+
+            modelBuilder
+                .Entity<Product>()
+                .HasMany(p => p.Sales)
+                .WithOne(s => s.Product);
+        }
+    }
+}
