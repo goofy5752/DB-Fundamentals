a8621885f37c50dfc7fb1a942de1d6ac3d9d4afe
Martin Asenov <talismanofkingdom@gmail.com>
2019-03-11 16:35:14 +0200

    Added new Code-First Project

---
+++ C# Databases Advanced/Code First/P01_HospitalDatabase.Data/Config.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_HospitalDatabase.Data
+{
+    public class Config
+    {
+        public const string SqlConnection = @"Server=DESKTOP-58UASJ8\SQLEXPRESS;Database=Hospital;Integrated Security=True";
+    }
+}
---
+++ C# Databases Advanced/Code First/P01_HospitalDatabase.Data/HospitalContext.cs
@@ -0,0 +1,169 @@
+﻿namespace P01_HospitalDatabase.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Models;
+
+    public class HospitalContext : DbContext
+    {
+        public DbSet<Patient> Patients { get; set; }
+
+        public DbSet<Visitation> Visitations { get; set; }
+
+        public DbSet<Diagnose> Diagnoses { get; set; }
+
+        public DbSet<Medicament> Medicaments { get; set; }
+
+        public DbSet<Doctor> Doctors { get; set; }
+
+        public DbSet<PatientMedicament> PatientMedicaments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Config.SqlConnection);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            ConfigurePatientEntity(modelBuilder);
+
+            ConfigureVisitationEntity(modelBuilder);
+
+            ConfigureDiagnoseEntity(modelBuilder);
+
+            ConfigureMedicamentEntity(modelBuilder);
+
+            ConfigurePatientMedicamentEntity(modelBuilder);
+
+            ConfigureDoctorEntity(modelBuilder);
+        }
+
+        private void ConfigureDoctorEntity(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Doctor>()
+                .HasKey(x => x.DoctorId);
+
+            modelBuilder
+                .Entity<Doctor>()
+                .HasMany(d => d.Visitations)
+                .WithOne(v => v.Doctor);
+
+            modelBuilder
+                .Entity<Doctor>()
+                .Property(x => x.Name)
+                .HasMaxLength(100)
+                .IsUnicode();
+
+            modelBuilder
+                .Entity<Doctor>()
+                .Property(x => x.Speciality)
+                .HasMaxLength(100)
+                .IsUnicode();
+        }
+
+        private void ConfigurePatientMedicamentEntity(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<PatientMedicament>()
+                .HasKey(x => new { x.PatientId, x.MedicamentId });
+
+            modelBuilder
+                .Entity<PatientMedicament>()
+                .HasOne(pm => pm.Patient)
+                .WithMany(p => p.Prescriptions)
+                .HasForeignKey(pm => pm.PatientId);
+
+            modelBuilder
+                .Entity<PatientMedicament>()
+                .HasOne(pm => pm.Medicament)
+                .WithMany(m => m.Prescriptions)
+                .HasForeignKey(pm => pm.MedicamentId);
+        }
+
+        private void ConfigureMedicamentEntity(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Medicament>()
+                .HasKey(x => x.MedicamentId);
+
+            modelBuilder
+                .Entity<Medicament>()
+                .Property(x => x.Name)
+                .HasMaxLength(50)
+                .IsUnicode();
+        }
+
+        private void ConfigureDiagnoseEntity(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Diagnose>()
+                .HasKey(x => x.DiagnoseId);
+
+            modelBuilder
+                .Entity<Diagnose>()
+                .Property(x => x.Name)
+                .HasMaxLength(50)
+                .IsUnicode();
+
+            modelBuilder
+                .Entity<Diagnose>()
+                .Property(x => x.Comments)
+                .HasMaxLength(250)
+                .IsUnicode();
+        }
+
+        private void ConfigureVisitationEntity(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Visitation>()
+                .HasKey(x => x.VisitationId);
+
+            modelBuilder
+                .Entity<Visitation>()
+                .Property(x => x.Comments)
+                .HasMaxLength(250)
+                .IsUnicode();
+        }
+
+        private void ConfigurePatientEntity(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Patient>()
+                .HasKey(x => x.PatientId);
+
+            modelBuilder
+                .Entity<Patient>()
+                .HasMany(p => p.Visitations)
+                .WithOne(v => v.Patient);
+
+            modelBuilder
+                .Entity<Patient>()
+                .HasMany(p => p.Diagnoses)
+                .WithOne(d => d.Patient);
+
+            modelBuilder
+                .Entity<Patient>()
+                .Property(x => x.FirstName)
+                .HasMaxLength(50)
+                .IsUnicode();
+
+            modelBuilder
+                .Entity<Patient>()
+                .Property(x => x.LastName)
+                .HasMaxLength(50)
+                .IsUnicode();
+
+            modelBuilder
+                .Entity<Patient>()
+                .Property(x => x.Address)
+                .HasMaxLength(250)
+                .IsUnicode();
+
+            modelBuilder
+                .Entity<Patient>()
+                .Property(x => x.Email)
+                .HasMaxLength(80)
+                .IsUnicode(false);
+        }
+    }
+}
