22bff08d82d410603f72eece76025be2b2dd0524
Martin Asenov <talismanofkingdom@gmail.com>
2019-03-12 17:29:27 +0200

    New StudentSystem entity added

---
+++ C# Databases Advanced/Entity Relations/P01_StudentSystem.Data.Models/Course.cs
@@ -0,0 +1,28 @@
+﻿namespace P01_StudentSystem.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Course
+    {
+        public Course(string name)
+        {
+            this.Name = name;
+            this.Resources = new List<Resource>();
+        }
+
+        public int CourseId { get; set; }
+
+        public string Name { get; private set; }
+
+        public string Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public decimal Price { get; set; }
+
+        public ICollection<Resource> Resources { get; set; }
+    }
+}
---
+++ C# Databases Advanced/Entity Relations/P01_StudentSystem.Data.Models/Student.cs
@@ -0,0 +1,23 @@
+﻿namespace P01_StudentSystem.Data.Models
+{
+    using System;
+
+    public class Student
+    {
+        public Student(string name)
+        {
+            this.Name = name;
+            this.RegisteredOn = DateTime.Now;
+        }
+
+        public int StudentId { get; set; }
+
+        public string Name { get; private set; }
+
+        public string PhoneNumber { get; set; }
+
+        public DateTime RegisteredOn { get; private set; }
+
+        public DateTime? Birthday { get; set; }
+    }
+}
