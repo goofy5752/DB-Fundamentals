f8ebfa175c554fdd5938ec9467fa13f008f37785
Martin Asenov <talismanofkingdom@gmail.com>
2019-03-13 14:54:40 +0200

    Added Football Betting System

---
+++ C# Databases Advanced/Entity Relations/P03_FootballBetting.Data/FootballBettingContext.cs
@@ -0,0 +1,37 @@
+﻿namespace P03_FootballBetting.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using P03_FootballBetting.Data.Models;
+
+    public class FootballBettingContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Config.SqlConnect);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            ConfigTeamEntity(modelBuilder);
+        }
+
+        private static void ConfigTeamEntity(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                            .Entity<Team>(entity =>
+                            {
+                                entity
+                        .HasKey(t => t.TeamId);
+
+                                entity
+                        .Property(t => t.Name)
+                        .HasMaxLength(25)
+                        .IsUnicode(false)
+                        .IsRequired();
+                            });
+        }
+    }
+}
