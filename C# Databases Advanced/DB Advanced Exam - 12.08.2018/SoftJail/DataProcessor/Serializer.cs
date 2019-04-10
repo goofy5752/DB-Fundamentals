f170326bbea22a4d88c47aecb2a1cd39698b0d2c
Martin Asenov <talismanofkingdom@gmail.com>
2019-04-10 23:47:24 +0300

    Refactoring

--- C# Databases Advanced/DB Advanced Exam - 12.08.2018/SoftJail/DataProcessor/Serializer.cs
+++ C# Databases Advanced/DB Advanced Exam - 12.08.2018/SoftJail/DataProcessor/Serializer.cs
@@ -3,8 +3,13 @@
 
     using Data;
     using Newtonsoft.Json;
+    using SoftJail.DataProcessor.ExportDto;
     using System;
+    using System.IO;
     using System.Linq;
+    using System.Text;
+    using System.Xml;
+    using System.Xml.Serialization;
 
     public class Serializer
     {
@@ -31,12 +36,56 @@
                 .ThenBy(x => x.Id)
                 .ToList();
 
-            return JsonConvert.SerializeObject(exportPrisonersByCells, Formatting.Indented);
+            return JsonConvert.SerializeObject(exportPrisonersByCells, Newtonsoft.Json.Formatting.Indented);
         }
 
         public static string ExportPrisonersInbox(SoftJailDbContext context, string prisonersNames)
         {
-            throw new NotImplementedException();
+            var prisonersInbox = context
+                .Prisoners
+                .Where(p => prisonersNames
+                .Split(",", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray()
+                .Contains(p.FullName))
+                .Select(p => new ExportInboxMessageForPrisonerDto()
+                {
+                    Id = p.Id,
+                    Name = p.FullName,
+                    IncarcerationDate = p.IncarcerationDate.ToString("yyyy-MM-dd"),
+                    EncryptedMessages = p.Mails.Select(m => new MessageDto()
+                    {
+                        Description = ReverseString(m.Description)
+                    })
+                    .ToArray()
+                })
+                .OrderBy(p => p.Name)
+                .ThenBy(p => p.Id)
+                .ToArray();
+
+            var xml = new XmlSerializer(typeof(ExportInboxMessageForPrisonerDto[]), new XmlRootAttribute("Prisoners"));
+
+            var sb = new StringBuilder();
+
+            var namespaces = new XmlSerializerNamespaces(new[]
+            {
+                new XmlQualifiedName("", "")
+            });
+
+            xml.Serialize(new StringWriter(sb), prisonersInbox, namespaces);
+
+            return sb.ToString().TrimEnd();
+        }
+
+        private static string ReverseString(string input)
+        {
+            var sb = "";
+
+            foreach (var item in input.Reverse())
+            {
+                sb += item;
+            }
+
+            return sb;
         }
     }
 }
